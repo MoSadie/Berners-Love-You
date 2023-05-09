10770cb5ecb26650ddc332063bc119230b705e8a
MoSadie <mosadie@mosadie.com>
2023-05-09 13:17:24 -0700

    MVP achieved

--- berners-love-you/src/App.js
+++ berners-love-you/src/App.js
@@ -1,15 +1,34 @@
 import logo from './logo.svg';
 import { useState } from 'react';
 import RedditEmbed from './components/RedditEmbed';
+import TwitterEmbed from './components/TwitterEmbed';
+import posts from './posts.json';
 import './App.css';
 
 function App() {
-  let [visible, setVisible] = useState(false);
+  function getRandomPost() {
+    return posts[Math.floor(Math.random() * posts.length)];
+  }
+
+  const [post, setPost] = useState(getRandomPost());
+
   return (
     <div className="App">
       <h1>Berners loves you!</h1>
-      <button onClick={() => setVisible(!visible)}>Toggle</button>
-      {visible && <RedditEmbed url="https://reddit.com/r/bernesemountaindogs/comments/ngfs33/im_wimth_friemd/" />}
+      <h3>Here is a Bernese Mountain Dog post to make you smile! 😄</h3>
+      <button type='button' onClick={() => setPost(getRandomPost())} style={{fontSize:16}}>New Post</button>
+      <br />
+      <div style={{padding: "10px"}}>
+      {post.startsWith("https://reddit.com") && <RedditEmbed key={post} url={post} />}
+      {post.startsWith("https://twitter.com") && <TwitterEmbed key={post} url={post} />}
+      </div>
+
+      <p>Link to original post:</p>
+      <p>
+        <a href={post}>{post}</a>
+      </p>
+      {/* {visible && <RedditEmbed url="https://reddit.com/r/bernesemountaindogs/comments/ngfs33/im_wimth_friemd/" />}
+      {!visible && <TwitterEmbed url="https://twitter.com/bunsenbernerbmd/status/1511328624191844353" />} */}
     </div>
   );
 }
--- berners-love-you/src/components/RedditEmbed.js
+++ berners-love-you/src/components/RedditEmbed.js
@@ -1,55 +1,30 @@
 import React from 'react';
 import { useEffect } from 'react';
 
-// class RedditEmbed extends React.Component {
-//     componentDidMount() {
-//         const script = document.createElement("script");
-//         script.src = "https://embed.redditmedia.com/widgets/platform.js";
-//         script.async = true;
-//         script.id = "reddit-embed-script";
-//         document.body.appendChild(script);
-//     }
-
-//     render() {
-//         return (
-//                     <div className="reddit-embed">
-//                         <blockquote id="embed" className="reddit-embed">
-//                             <a id="post" href={url}>{url}</a>
-//                         </blockquote>
-//                     </div>
-//                 );
-//         }
-
-//     componentWillUnmount() {
-//         // Whatever you need to cleanup the widgets code
-//         document.getElementById("reddit-embed-script").remove();
-//     }
-// }
-
 function RedditEmbed({ url }) {
     // On load, create the script tag and insert it into the page
     useEffect(() => {
     const script = document.createElement("script");
-    script.src = "https://embed.redditmedia.com/widgets/platform.js";
+    script.src = "https://embed.reddit.com/widgets.js";
     script.async = true;
     script.id = "reddit-embed-script";
     document.body.appendChild(script);
-    
-    //Cleanup code
+
     return () => {
+        // Whatever you need to cleanup the widgets code
         document.getElementById("reddit-embed-script").remove();
     }
     }, []);
     
 
     return (
-        <><div className="reddit-embed">
+        <div>
+        <div className="reddit-embed">
         <blockquote id="embed" className="reddit-embed">
         <a id="post" href={url}>{url}</a>
         </blockquote>
         </div>
-        <script src="https://embed.redditmedia.com/widgets/platform.js" charSet="UTF-8"></script>
-        </>
+        </div>
     );
 }
     
