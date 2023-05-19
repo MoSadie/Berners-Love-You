370212c0ed1fdc8e4a2ca252f2064930a2f48f8c
MoSadie <mosadie@mosadie.com>
2023-05-19 14:44:12 -0700

    Add deletion check + command

--- bly-validator/index.js
+++ bly-validator/index.js
@@ -1,19 +1,19 @@
-const args = require('args');
+const args = require("args");
 
 args
-    .option('post', 'The URL of the post to validate')
-    .option('platform', 'The platform to validate the post for')
-    .option('existing', 'The file with existing posts to update.')
-    .option('output', 'The output file to write the results to')
+  .option("post", "The URL of the post to validate")
+  .option("platform", "The platform to validate the post for")
+  .option("existing", "The file with existing posts to update.")
+  .option("output", "The output file to write the results to");
 
 const flags = args.parse(process.argv);
 
 if (!flags.post || !flags.platform || !flags.existing || !flags.output) {
-    args.showHelp();
+  args.showHelp();
 }
 
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 const existingPath = path.resolve(flags.existing);
 const outputPath = path.resolve(flags.output);
@@ -21,50 +21,47 @@ const outputPath = path.resolve(flags.output);
 const existing = require(existingPath);
 
 if (!existing) {
-    console.log('Invalid existing posts file');
-    process.exit(1);
+  console.log("Invalid existing posts file");
+  process.exit(1);
 }
 
 const platforms = {
-    'twitter': require('./platforms/twitter'),
-    'reddit': require('./platforms/reddit'),
-}
+  twitter: require("./platforms/twitter"),
+  reddit: require("./platforms/reddit"),
+};
 
 const platform = platforms[flags.platform];
 
 if (!platform) {
-    console.log('Invalid platform');
-    process.exit(1);
+  console.log("Invalid platform");
+  process.exit(1);
 }
 
 const post = flags.post;
 
-const validated = platform.validate(post);
-
-if (!validated) {
-    console.log('Post is not valid');
+platform.validate(post).then((validated) => {
+  if (!validated) {
+    console.log("Post is not valid");
     process.exit(1);
-}
+  }
 
-const existingPost = existing.find(p => p.url === post);
+  const existingPost = existing.find((p) => p.url === post);
 
-if (existingPost) {
-    console.log('Post already exists');
+  if (existingPost) {
+    console.log("Post already exists");
     process.exit(1);
-}
+  }
 
-// Combine the validated post with the existing posts
-const postData = {
+  // Combine the validated post with the existing posts
+  const postData = {
     url: post,
     platform: flags.platform,
-}
+  };
 
-const combined = [
-    ...existing,
-    postData,
-]
+  const combined = [...existing, postData];
 
-// Write the combined posts to the output file
-fs.writeFileSync(outputPath, JSON.stringify(combined, null, 2));
+  // Write the combined posts to the output file
+  fs.writeFileSync(outputPath, JSON.stringify(combined, null, 2));
 
-console.log('Post added successfully');
+  console.log("Post added successfully");
+});
--- bly-validator/platforms/twitter.js
+++ bly-validator/platforms/twitter.js
@@ -1,5 +1,7 @@
+const { isDeleted } = require("./reddit");
+
 const twitter = {
-    validate: (post) => {
+    validate: async (post) => {
         // Check if post begins with https://twitter.com/ or https://www.twitter.com/
         // and ends with /status/*
         // If so, return true
@@ -7,7 +9,36 @@ const twitter = {
 
         const regex = /^https?:\/\/(www\.)?twitter\.com\/.*\/status\/.*$/;
 
-        return regex.test(post);
+        const regexResult = regex.test(post);
+
+        if (!regexResult) {
+            return false;
+        }
+
+        // Commented out so validator can work at all, blame Twitter's API.
+        // var isDeleted = await twitter.isDeleted(post);
+
+        return true;
+    },
+
+    isDeleted: async (post) => {
+        // Check if post is deleted,
+        // Returns true if deleted or
+        // null if an error occurred (ex invalid url)
+
+        // Check if post begins with https://twitter.com/ or https://www.twitter.com/
+        // and ends with /status/*
+
+        const regex = /^https?:\/\/(www\.)?twitter\.com\/.*\/status\/.*$/;
+        const regexResult = regex.test(post);
+
+        if (!regexResult) {
+            return null;
+        }
+
+        console.log("Unfortunately, Twitter's API does not allow me to check if a post is deleted for free, sorry.");
+
+        return null;
     }
 }
 
