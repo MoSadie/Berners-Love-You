42654c1fc23b0d89adc2b23c78b3b7cd6ac3fe92
MoSadie <mosadie@mosadie.com>
2024-12-14 02:27:45 -0800

    An idea
    Attempting to use the client JS to do OAuth so I can retrieve saved posts in a better way. Untested.

--- berners-love-you/src/App.js
+++ berners-love-you/src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import RedditEmbed from './components/RedditEmbed';
 import TwitterEmbed from './components/TwitterEmbed';
 import posts from './posts.json';
+import snoowrap from 'snoowrap';
 import './App.css';
 
 function getPostComponent(post) {
@@ -25,6 +26,54 @@ function App() {
   }
 
   const [post, setPost] = useState(getRandomPost());
+
+  // Check if query contains "reddit"
+  if (window.location.search.includes("reddit")) {
+    // If parameter state is set, attempt to show Reddit Saved tool instead
+    if (window.location.search.includes("access_token")) {
+      var r = new snoowrap({
+        userAgent: 'Berners-Love-You',
+        clientId: "FIXME", //FIXME
+        clientSecret: "",
+        accessToken: window.location.search.split("access_token=")[1].split("&")[0]
+      });
+
+      // Get saved posts to export as JSON
+      r.getMe().getSavedContent().fetchAll({skipReplies: true}).then((posts) => {
+        var savedPosts = [];
+        posts.forEach((post) => {
+          savedPosts.push({
+            platform: "reddit",
+            url: post.url
+          });
+        });
+
+        console.log(JSON.stringify(savedPosts));
+        alert("Saved posts have been exported to the console. Please check the console for the JSON output.");
+      });
+
+      return (
+        <div className="App">
+          <h1>Exporting Reddit Saved Posts...</h1>
+        </div>
+      );
+    } else {
+      // Redirect to Reddit OAuth
+      window.location.href = snoowrap.getAuthUrl({
+        clientId: "FIXME", //FIXME
+        scope: ['history', 'identity'],
+        permanent: false,
+        redirectUri: window.location,
+        state: "redditdogs"
+      });
+
+      return (
+        <div className="App">
+          <h1>Redirecting to Reddit...</h1>
+        </div>
+      );
+    }
+  }
 
   return (
     <div className="App">
